5d50d88986e324be5dd0c14c3b4bd9ef565157d5
Teodora <nikolic8teodora@gmail.com>
2026-05-29 10:11:50 +0200

    Update login page styling

--- smartvolley-react/src/pages/Login.jsx
+++ smartvolley-react/src/pages/Login.jsx
@@ -25,7 +25,6 @@ export default function Login() {
 
     const data = await res.json();
 
-
     if (res.status === 422) {
       setErrors(data.errors);
     } else if (res.status === 401) {
@@ -39,38 +38,40 @@ export default function Login() {
   }
 
   return (
-    <>
-      <h1 className="title"> Prijavite se na svoj nalog</h1>
-      <form onSubmit={handleLogin} className="login-form">
-        <div>
-          <input
-            className="login-input"
-            type="text"
-            placeholder="Korisničko ime"
-            value={formData.username}
-            onChange={(e) => {
-              setFormData({ ...formData, username: e.target.value });
-            }}
-          />
-          {errors.username && <p className="error">{errors.username[0]}</p>}
-        </div>
-        <div>
-          <input
-            className="login-input"
-            type="password"
-            placeholder="Lozinka"
-            value={formData.password}
-            onChange={(e) => {
-              setFormData({ ...formData, password: e.target.value });
-            }}
-          />
-          {errors.password && <p className="error">{errors.password[0]}</p>}
-        </div>
-        <div>
-          {errors.general && <p className="error">{errors.general}</p>}
-          <button className="login-button">Prijavi se</button>
-        </div>
-      </form>
-    </>
+    <div className="login-page">
+      <div className="login-card">
+        <h1 className="title">Prijavite se na svoj nalog</h1>
+        <form onSubmit={handleLogin} className="login-form">
+          <div>
+            <input
+              className="login-input"
+              type="text"
+              placeholder="Korisničko ime"
+              value={formData.username}
+              onChange={(e) => {
+                setFormData({ ...formData, username: e.target.value });
+              }}
+            />
+            {errors.username && <p className="error">{errors.username[0]}</p>}
+          </div>
+          <div>
+            <input
+              className="login-input"
+              type="password"
+              placeholder="Lozinka"
+              value={formData.password}
+              onChange={(e) => {
+                setFormData({ ...formData, password: e.target.value });
+              }}
+            />
+            {errors.password && <p className="error">{errors.password[0]}</p>}
+          </div>
+          <div>
+            {errors.general && <p className="error">{errors.general}</p>}
+            <button className="login-button">Prijavi se</button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
